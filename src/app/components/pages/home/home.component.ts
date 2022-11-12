fc5860c4895068ea7612c74eabfd8f93f1a173d1
Dipak <sinhadipak33@gmail.com>
2022-11-12 20:23:05 +0530

    Added search food functionality

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from 'src/app/shared/models/Food';
 import { FoodService } from 'src/app/services/food.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,8 +10,13 @@ import { FoodService } from 'src/app/services/food.service';
 export class HomeComponent implements OnInit {
 
   foods: Food[] = [];
-  constructor(private foodService:FoodService) { 
-    this.foods=foodService.getAll();//foodservice=new FoodService();
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = foodService.getAllFoodsBySearchTerm(params.searchTerm)
+      else
+        this.foods = foodService.getAll();//foodservice=new FoodService();
+    })
   }
   ngOnInit(): void {
   }
